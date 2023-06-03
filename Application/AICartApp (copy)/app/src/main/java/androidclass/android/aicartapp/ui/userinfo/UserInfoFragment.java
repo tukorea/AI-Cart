32eb7fe0a92433486fcc7d4c2c0c5d0fc229f1ee
yuningyun <ing6264@naver.com>
2023-06-03 10:50:39 +0900

    app_success1

---
+++ Application/AICartApp (copy)/app/src/main/java/androidclass/android/aicartapp/ui/userinfo/UserInfoFragment.java
@@ -0,0 +1,38 @@
+package androidclass.android.aicartapp.ui.userinfo;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidclass.android.aicartapp.R;
+
+public class UserInfoFragment extends Fragment {
+
+    private UserInfoViewModel mViewModel;
+
+    public static UserInfoFragment newInstance() {
+        return new UserInfoFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user_info, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(UserInfoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
